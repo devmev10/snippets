e53734ec50fa7a0fc3d2abd8381c3ba9e87034da
Ravi Tokas <ravitokas90@gmail.com>
2024-06-14 14:13:53 +0530

    update page.tsx

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,11 +1,23 @@
 import { db } from "@/db";
+import { redirect } from "next/navigation";
 
 export default function SnippetCreatePage() {
   async function createSnippet(formData: FormData) {
     "use server";
 
-    const title = formData.get("title");
-    const code = formData.get("code");
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    const snippet = await db.snippet.create({
+      data: {
+        title: title,
+        code: code,
+      },
+    });
+
+    console.log(snippet);
+
+    redirect("/");
   }
 
   return (
