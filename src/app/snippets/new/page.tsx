3736db9e0ede008625ebfe87a1a5c76736c1d282
Ravi Tokas <ravitokas90@gmail.com>
2024-06-14 14:14:28 +0530

    update page.tsx

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -21,7 +21,7 @@ export default function SnippetCreatePage() {
   }
 
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
