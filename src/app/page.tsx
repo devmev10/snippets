ff5e93dc082e3a2372e5a51d2f561b191ec91219
Ravi Tokas <ravitokas90@gmail.com>
2024-06-15 14:02:18 +0530

    update dev.db and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,9 @@ import { db } from "@/db";
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
-  return <div>Home page</div>;
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+
+  return <div>{renderedSnippets}</div>;
 }
