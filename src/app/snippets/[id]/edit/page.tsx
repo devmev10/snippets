8c84138db2fda5b178fe0f7c32a4b36da597c8cf
Ravi Tokas <ravitokas90@gmail.com>
2024-06-18 12:43:50 +0530

    fetch snippet based  on id from prisma db

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -8,5 +8,7 @@ interface SnippetEditPageProps {
 
 export default async function SnippetEditPage(props: SnippetEditPageProps) {
   const id = parseInt(props.params.id);
+  const snippet = await db.snippet.findFirst({ where: { id: id } });
+
   return <div>Editing snippet with id {id}</div>;
 }
